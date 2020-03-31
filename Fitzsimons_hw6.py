6f502dfde562f34c7e0fb98089bcdff51e3749b6
aidanzfitz <62619898+aidanzfitz@users.noreply.github.com>
2020-03-31 01:39:00 -0400

    updated error graphs
    updated error graphs for FE, CN, BE methods, confirmed correct slopes

--- Fitzsimons_hw6.py
+++ Fitzsimons_hw6.py
@@ -95,9 +95,6 @@ RK4: fourth order Runge-Kutta
 def RK4(w0, z, m, w, x0, T, N):
     A = np.matrix([[0,1],[(-1*(w0**2)),(-2*w0*z)]])
     dt = T/N
-    tA = np.multiply((dt/2),A)
-    I = np.matrix([[1,0],[0,1]])
-    ItA = np.linalg.inv((I - tA))
     xvec = [[0 for i in range(N+1)] for j in range(2)]
     t = list(range(N+1))
     x = [0] * (N+1)
@@ -106,8 +103,31 @@ def RK4(w0, z, m, w, x0, T, N):
     xvec[0][0] = x0[0]
     xvec[1][0] = x0[1]
     for ii in range(N):
-        for ii in range(len(xvec[0])):
-            x[ii] = float(xvec[0][ii])
+        xn = np.matrix([[float(xvec[0][ii])],[float(xvec[1][ii])]])
+        bn = ([[0],[math.cos(w*t[ii])/m]])
+        k1 = (xn + np.multiply(dt, np.matmul(A,xn)) + np.multiply(dt,bn))
+
+        #k2 add step
+        addstep = xn + (k1/2)
+        k2 = (addstep + np.multiply(dt/2, np.matmul(A,addstep))
+              + np.multiply(dt/2,bn))
+
+        #k3 add step
+        addstep = xn + (k2/2)
+        k3 = (addstep + np.multiply(dt/2, np.matmul(A,addstep))
+              + np.multiply(dt/2,bn))
+
+        #k4 addstep
+        addstep = xn + k3
+        k4 = (addstep + np.multiply(dt, np.matmul(A,addstep))
+              + np.multiply(dt,bn))
+
+        xn1 = xn + (1/6)*(k1 + k2 + k2 + k3 + k3 + k4)
+        
+        xvec[0][ii+1] = float(xn1[0]) 
+        xvec[1][ii+1] = float(xn1[1])
+    for ii in range(len(xvec[0])):
+        x[ii] = float(xvec[0][ii])
     return (x,t)
 
 """
@@ -124,19 +144,50 @@ if __name__ == '__main__':
     FElast = [0] * 5
     BElast = [0] * 5
     CNlast = [0] * 5
+    RKlast = [0] * 5
+    answer = 0.5*(math.sin(10)-(10*math.exp(-10)))
+    print(answer)
+
     for i in range(len(N)):
         n = N[i]
         [x,t] = FE(w0,z,m,w,x0,T,n)
-        FElast[i] = x[len(x)-1]
+        FElast[i] = abs(x[len(x)-1] - answer)
         [x,t] = BE(w0,z,m,w,x0,T,n)
-        BElast[i] = x[len(x)-1]
+        BElast[i] = abs(x[len(x)-1] - answer)
         [x,t] = CN(w0,z,m,w,x0,T,n)
-        CNlast[i] = x[len(x)-1]
+        CNlast[i] = abs(x[len(x)-1] - answer)
     print(FElast)
     print(BElast)
     print(CNlast)
 
-    answer = 0.5*(math.sin(10)-(10*math.exp(-10)))
-    print(answer)
-            
+    plt.figure()
+    fig, ax = plt.subplots()
+    ax.plot(np.log10(N), np.log10(FElast), label = 'Error')
+    legend = ax.legend(loc = 'upper left')
+    plt.title('Error using FE estimate')
+    plt.xlabel('N (log base 10)')
+    plt.ylabel('Error (log base 10)')
+    plt.savefig('FE.png',bbox_inches = 'tight')
+    plt.close()
+
+    plt.figure()
+    fig, ax = plt.subplots()
+    ax.plot(np.log10(N), np.log10(BElast), label = 'Error')
+    legend = ax.legend(loc = 'upper left')
+    plt.title('Error using BE estimate')
+    plt.xlabel('N (log base 10)')
+    plt.ylabel('Error (log base 10)')
+    plt.savefig('BE.png',bbox_inches = 'tight')
+    plt.close()
+
+    plt.figure()
+    fig, ax = plt.subplots()
+    ax.plot(np.log10(N), np.log10(CNlast), label = 'Error')
+    legend = ax.legend(loc = 'upper left')
+    plt.title('Error using CN estimate')
+    plt.xlabel('N (log base 10)')
+    plt.ylabel('Error (log base 10)')
+    plt.savefig('CN.png',bbox_inches = 'tight')
+    plt.close()
+                    
     
